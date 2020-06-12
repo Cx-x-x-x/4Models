bfe729b051e650482c865ff4f0b5e6f57c362ad0
Cx_x_x <1004058940@qq.com>
2020-06-12 17:18:02 +0800

    修改 filter_weight_decay.py

--- filter_weight_decay.py
+++ filter_weight_decay.py
@@ -1,25 +1,15 @@
-import torch.nn as nn
-
-
 def group_weight(model):
     group_decay = []
     group_no_decay = []
-    for m in model.modules():
-        if isinstance(m, nn.Linear):
-            group_decay.append(m.weight)
-            if m.bias is not None:
-                group_no_decay.append(m.bias)
-        elif isinstance(m, nn.Conv2d):
-            group_decay.append(m.weight)
-            if m.bias is not None:
-                group_no_decay.append(m.bias)
-        elif isinstance(m, nn.BatchNorm2d):
-            if m.bias is not None:
-                group_no_decay.append(m.weight)
-            if m.bias is not None:
-                group_no_decay.append(m.bias)
+    for name, param in model.named_parameters():
+        # if ('conv' and 'weight') in name:  # = conv or weight in name why?????????
+        if 'conv' in name and 'weight' in name:
+            group_decay.append(param)
+            # print('decay:', name)
+        else:
+            group_no_decay.append(param)
+            # print('no_decay:', name)
 
     assert len(list(model.parameters())) == len(group_decay) + len(group_no_decay)
     groups = [dict(params=group_decay), dict(params=group_no_decay, weight_decay=.0)]
     return groups
-
